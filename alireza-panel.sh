575d9de6bf07e60f80289b10d2dbdfc25c052b89
m3hdiclub <132158452+m3hdiclub@users.noreply.github.com>
2024-03-16 14:12:50 +0330

    Update alireza-panel.sh

--- alireza-panel.sh
+++ alireza-panel.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# Update package lists
-apt update -y
-
-# Upgrade installed packages
-apt upgrade -y
-
-# Install x-ui from GitHub
-bash <(curl -Ls https://raw.githubusercontent.com/alireza0/x-ui/master/install.sh)
+sudo apt update -y && sudo apt upgrade -y
