f229fee528883a2fd2553f6e5ea5fb112a849617
m3hdiclub <132158452+m3hdiclub@users.noreply.github.com>
2024-08-05 11:23:40 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,15 +35,17 @@ display_main_menu() {
     echo
     yellow "-----------------INSTALL----------------------------"
     echo
-    green "1. UPDATE                2. UPDATE2"
+    green "1. UPDATE"
+    echo
+    green "2. Change SSH            3. UFW"
     echo
     yellow "--------------------Panels-----------------------------"
     echo
-    green "3. S-UI                  4. H-UI"
+    green "4. S-UI                  5. H-UI"
     echo
-    green "5. X-UI [3x]                  6. X-UI [alireza]"
+    green "6. X-UI [3x]             7. X-UI [alireza]"
     echo
-    rred "0. Exit"
+    rred  "0. Exit"
     echo "------------------------------------------------------"
 }
 
@@ -88,29 +90,83 @@ update() {
     done
 }
 
-update2() {
-    echo "Executing UPDATE2..."
-    # دستوراتی که مربوط به گزینه 2 هستند
+change_ssh() {
+    while true; do
+        echo "$(green "Installing net-tools...")"
+        
+        # نصب بسته net-tools
+        sudo apt install net-tools -y
+        
+        # بررسی موفقیت نصب net-tools
+        if [ $? -eq 0 ]; then
+            echo "$(green "net-tools installed successfully.")"
+            
+            echo "$(green "Opening SSH configuration file...")"
+            
+            # باز کردن فایل پیکربندی SSH با nano
+            sudo nano /etc/ssh/sshd_config
+            
+            # بررسی موفقیت خروج از nano و ذخیره تغییرات
+            if [ $? -eq 0 ]; then
+                echo "$(green "Configuration file edited successfully.")"
+                
+                echo "$(green "Restarting SSH service...")"
+                
+                # راه‌اندازی مجدد سرویس SSH
+                sudo systemctl restart sshd.service
+                
+                # بررسی موفقیت راه‌اندازی مجدد سرویس
+                if [ $? -eq 0 ]; then
+                    echo "$(green "SSH service restarted successfully.")"
+                else
+                    echo "$(red "Failed to restart SSH service.")"
+                fi
+            else
+                echo "$(red "Failed to edit SSH configuration file.")"
+            fi
+        else
+            echo "$(red "Failed to install net-tools.")"
+        fi
+
+        # بررسی وضعیت نصب
+        read -p "$(yellow "Is the SSH configuration correct? (y/n): ")" answer
+        case $answer in
+            y|Y)
+                echo "$(green "Returning to the menu...")"
+                break ;;
+            n|N)
+                echo "$(red "Reinstalling...")"
+                ;;
+            *)
+                echo "$(red "Invalid input. Please type y or n.")"
+                ;;
+        esac
+    done
+}
+
+ufw() {
+    echo "Executing UFW..."
+    # دستوراتی که مربوط به گزینه 3 هستند
 }
 
 s_ui() {
     echo "Executing S-UI..."
-    # دستوراتی که مربوط به گزینه 3 هستند
+    # دستوراتی که مربوط به گزینه 4 هستند
 }
 
 h_ui() {
     echo "Executing H-UI..."
-    # دستوراتی که مربوط به گزینه 4 هستند
+    # دستوراتی که مربوط به گزینه 5 هستند
 }
 
 x_ui_3x() {
     echo "Executing X-UI [3x]..."
-    # دستوراتی که مربوط به گزینه 5 هستند
+    # دستوراتی که مربوط به گزینه 6 هستند
 }
 
 x_ui_alireza() {
     echo "Executing X-UI [alireza]..."
-    # دستوراتی که مربوط به گزینه 6 هستند
+    # دستوراتی که مربوط به گزینه 7 هستند
 }
 
 exit_script() {
@@ -124,11 +180,12 @@ while true; do
     read -p "$(yellow "Select an option: ")" option
     case $option in
         1) update ;;
-        2) update2 ;;
-        3) s_ui ;;
-        4) h_ui ;;
-        5) x_ui_3x ;;
-        6) x_ui_alireza ;;
+        2) change_ssh ;;
+        3) ufw ;;
+        4) s_ui ;;
+        5) h_ui ;;
+        6) x_ui_3x ;;
+        7) x_ui_alireza ;;
         0) exit_script ;;
         *) echo "$(red "Invalid option!")" ;;
     esac
