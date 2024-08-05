1bcf64513e004126a216386b1c88df951f5c849b
m3hdiclub <132158452+m3hdiclub@users.noreply.github.com>
2024-08-05 10:17:05 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,78 +1,126 @@
 #!/bin/bash
-
-# تعریف رنگ‌ها
 red='\033[0;31m'
-green='\033[0;32m'
+bblue='\033[0;34m'
 yellow='\033[0;33m'
+green='\033[0;32m'
 plain='\033[0m'
+red(){ echo -e "\033[31m\033[01m$1\033[0m";}
+green(){ echo -e "\033[32m\033[01m$1\033[0m";}
+yellow(){ echo -e "\033[33m\033[01m$1\033[0m";}
+blue(){ echo -e "\033[36m\033[01m$1\033[0m";}
+white(){ echo -e "\033[37m\033[01m$1\033[0m";}
+bblue(){ echo -e "\033[34m\033[01m$1\033[0m";}
+rred(){ echo -e "\033[35m\033[01m$1\033[0m";}
+readtp(){ read -t5 -n26 -p "$(yellow "$1")" $2;}
+readp(){ read -p "$(yellow "$1")" $2;}
 
-# تابع برای نمایش پیام‌ها با رنگ
-print_red() { echo -e "${red}$1${plain}"; }
-print_green() { echo -e "${green}$1${plain}"; }
-print_yellow() { echo -e "${yellow}$1${plain}"; }
+# install requirements
+if ! command -v qrencode &> /dev/null || ! command -v jq &> /dev/null || ! dpkg -l | grep -q net-tools; then
+    sudo apt-get update
+    if ! command -v qrencode &> /dev/null; then
+        rred "qrencode is not installed. Installing..."
+        sudo apt-get install qrencode -y
 
-# بررسی و نصب نرم‌افزارهای مورد نیاز
-install_requirements() {
-    if ! command -v curl &> /dev/null; then
-        print_red "curl نصب نشده است. در حال نصب..."
-        sudo apt-get update
-        if sudo apt-get install curl -y; then
-            print_green "curl با موفقیت نصب شد."
+        # Check if installation was successful
+        if [ $? -eq 0 ]; then
+            red "qrencode is now installed."
         else
-            print_red "خطا: نصب curl شکست خورد."
+            red "Error: Failed to install qrencode."
+        fi
+    else
+        green "qrencode is already installed."
+    fi
+
+    if ! command -v jq &> /dev/null; then
+        rred "Installing jq..."
+        if sudo apt-get install jq -y; then
+            red "jq installed successfully."
+        else
+            red "Error: Failed to install jq."
             exit 1
         fi
     else
-        print_green "curl از قبل نصب شده است."
+        green "jq is already installed."
     fi
 
-    if ! command -v git &> /dev/null; then
-        print_red "git نصب نشده است. در حال نصب..."
-        sudo apt-get update
-        if sudo apt-get install git -y; then
-            print_green "git با موفقیت نصب شد."
+    if ! dpkg -l | grep -q net-tools; then
+        rred "net-tools is not installed. Installing..."
+        sudo apt-get install net-tools -y
+
+        # Check if installation was successful
+        if [ $? -eq 0 ]; then
+            red "net-tools is now installed."
         else
-            print_red "خطا: نصب git شکست خورد."
+            red "Error: Failed to install net-tools."
             exit 1
         fi
     else
-        print_green "git از قبل نصب شده است."
+        green "net-tools is already installed."
     fi
-}
+else
+    green "qrencode, jq, and net-tools are already installed."
+fi
 
-# نمایش منوی اصلی
+# ----------------------------------------Show Menus------------------------------------------------
 display_main_menu() {
-    clear
+    clear 
     echo
-    print_yellow "----------------- منوی اصلی ---------------------"
-    print_green "1. نمایش وضعیت سیستم"
-    print_green "2. به‌روزرسانی سیستم"
-    print_green "3. خروج"
-    echo "--------------------------------------------------"
-    read -p "لطفاً گزینه‌ای را انتخاب کنید: " choice
-
-    case $choice in
-        1)
-            print_yellow "وضعیت سیستم:"
-            uname -a
-            ;;
-        2)
-            print_yellow "در حال به‌روزرسانی سیستم..."
-            sudo apt-get update && sudo apt-get upgrade -y
-            ;;
-        3)
-            print_red "خروج از برنامه"
-            exit 0
-            ;;
-        *)
-            print_red "گزینه نادرست است. لطفاً مجدداً امتحان کنید."
-            display_main_menu
-            ;;
-    esac
+    echo
+    bblue "                 █████╗ ██╗ ██████╗              "
+    bblue "                ██╔══██╗██║██╔═══██╗             "
+    bblue "                ███████║██║██║   ██║             "
+    bblue "                ██╔══██║██║██║   ██║             "
+    bblue "                ██║  ██║██║╚██████╔╝             "
+    bblue "                ╚═╝  ╚═╝╚═╝ ╚═════╝              "
+    bblue "               All-in-one Proxy Tool             "
+    white "                  Created by Hosy                "
+    white "------------------------------------------------------"
+    white " Github: https://github.com/hrostami"
+    white " Twitter: https://twitter.com/hosy000"
+    echo
+    echo -e "${plain}Thanks ${red}iSegaro${plain} for all the tutorials and new methods! "
+    echo
+    yellow "-----------------Protocols----------------------------"
+    green "1. Chisel Tunnel         2. Hysteria V2"
+    echo
+    green "3. Tuic                  4. Naive"
+    echo
+    green "5. SSH                   6. Reality+Tuic+Hy2+ws+Argo"
+    echo
+    yellow "--------------------Tools-----------------------------"
+    green "7. Reverse TLS Tunnel    8. Install Panels"
+    echo
+    green "9. Warp                  10. Telegram Proxy"
+    echo
+    green "11. Show used Ports      12. Set Domains"
+    echo
+    green "13. DNS(SmartSNI)        14. Hiddify Reality Scanner"
+    echo
+    rred "0. Exit"
+    echo "------------------------------------------------------"
 }
 
-# اجرای تابع نصب وابستگی‌ها
-install_requirements
-
-# نمایش منوی اصلی
-display_main_menu
+display_dns_menu() {
+    clear
+    echo "------------------------------------------------------"
+    echo -e "${plain}SmartSNI is created by ${yellow}bepass-org${plain}"
+    echo -e "Please check out and ${yellow}star ${plain}his Github repo"
+    echo -e "${yellow}https://github.com/bepass-org/smartSNI${plain}"
+    echo "------------------------------------------------------"
+    echo
+    echo "**********************************************"
+    yellow "                DNS Menu                 "
+    echo "**********************************************"
+    green "1. Auto Install"
+    echo
+    green "2. Add website"
+    echo
+    green "0. Back to Main Menu"
+    echo "**********************************************"
+    JSON_FILE="/root/smartSNI/config.json"
+    existing_domains=$(jq -r '.domains | keys[]' "$JSON_FILE")
+    yellow "Existing domains: "
+    white "$existing_domains"
+    echo
+    echo "**********************************************"
+}
