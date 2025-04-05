93dd8b1e782c69339c20e4b92877757fa675e0c5
m3hdiclub <132158452+m3hdiclub@users.noreply.github.com>
2025-04-05 16:41:03 +0330

    Update installnew.sh

--- installnew.sh
+++ installnew.sh
@@ -43,7 +43,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Downloading the ZIP file..."
-curl -L -o /root/file.zip "https://drive.google.com/uc?id=1b_jMbEgcCnh2HEIub6tLUppK6dCeUIyn&export=download"
+curl -L -o /root/m3hdiclub/bash/file.zip "https://drive.google.com/uc?id=16HGnrrpe4Fl2AtBBIc10SKUw-D8JGyy9&export=download#m3hdiclub"
 
 if [ $? -ne 0 ]; then
     echo "Failed to download the ZIP file."
@@ -51,29 +51,33 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Extracting the ZIP file..."
-sudo unzip /root/file.zip -d /root
+sudo unzip /root/m3hdiclub/bash/file.zip -d /root/m3hdiclub/bash
 if [ $? -ne 0 ]; then
     echo "Failed to unzip the file."
     exit 1
 fi
 
 echo "Removing the ZIP file..."
-rm /root/file.zip
+rm /root/m3hdiclub/bash/file.zip
 if [ $? -ne 0 ]; then
     echo "Failed to remove the ZIP file."
     exit 1
 fi
 
 echo "Changing permissions for m3hdiclub.sh..."
-sudo chmod +x /root/m3hdiclub.sh
+sudo chmod +x /root/m3hdiclub/bash/menu.sh
+sudo chmod +x /root/m3hdiclub/bash/ufw.sh
+sudo chmod +x /root/m3hdiclub/bash/ssh.sh
+sudo chmod +x /root/m3hdiclub/bash/dns.sh
+sudo chmod +x /root/m3hdiclub/bash/s-ui.sh
 if [ $? -ne 0 ]; then
     echo "Failed to change permissions."
     exit 1
 fi
 
 clear
-echo "Executing m3hdiclub.sh..."
-sudo /root/m3hdiclub.sh
+echo "Executing menu.sh..."
+sudo /root/m3hdiclub/bash/menu.sh
 if [ $? -ne 0 ]; then
     echo "Failed to execute m3hdiclub.sh."
     exit 1
